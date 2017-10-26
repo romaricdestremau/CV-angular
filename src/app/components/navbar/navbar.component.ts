85069440820029c217c131a2c4434d4558ec1218
Romaric Destremau <romaric.Destremau@gmail.com>
2017-10-26 02:19:22 +0200

    Ajout du navbar qui toggle quand on scroll, et du header comme avant

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, HostListener, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public navIsFixed: boolean = false;
+  public PageIsTop: boolean = true;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+
+
+  // Doc http://brianflove.com/2016/10/10/angular-2-window-scroll-event-using-hostlistener/
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 42) {
+      this.PageIsTop = false;
+    } else{
+      this.PageIsTop = true;
+    }
+    console.log('host listening is happenning');
   }
 
 }
