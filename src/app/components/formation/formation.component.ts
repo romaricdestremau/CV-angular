15af42df7d34a9a9813977a29edb1bfd74ceb737
Romaric Destremau <romaric.Destremau@gmail.com>
2017-10-30 00:20:20 +0100

    ajout de ng bootstrap, barre de progression

--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import {NgbProgressbarConfig} from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-formation',
   templateUrl: './formation.component.html',
-  styleUrls: ['./formation.component.css']
+  styleUrls: ['./formation.component.css'],
+  providers: [NgbProgressbarConfig]
 })
 export class FormationComponent implements OnInit {
   formations:Formation[];
   experiences:Experience[];
   competences:Competences[];
 
-  constructor(private dataService:DataService) { }
+  constructor(private dataService:DataService, config: NgbProgressbarConfig) {
+    config.max = 1000;
+      config.striped = true;
+      config.animated = true;
+      config.type = 'success'; }
 
   ngOnInit() {
     this.dataService.getFormations().subscribe((formations) => {this.formations = formations;}),
